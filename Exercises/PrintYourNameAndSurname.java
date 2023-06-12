4d818ad0fa4cd67188065ff64743f471c87c0a36
kylerobins <darkspidersteewy@gmail.com>
2023-06-12 15:52:31 +0300

    Input and output -2

--- Exercises/PrintYourNameAndSurname.java
+++ Exercises/PrintYourNameAndSurname.java
@@ -2,7 +2,7 @@ package Exercises;
 
 public class PrintYourNameAndSurname {
     public static void main(String[] args){
-        System.out.print("My surname is ");
+        System.out.print("My Name is Kyle robins and surname is ");
         System.out.print("Mutuma");
     }
 }
