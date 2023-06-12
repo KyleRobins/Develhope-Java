b4158ebc2138b4bd5db82efa71dcad5d78ba9c76
kylerobins <darkspidersteewy@gmail.com>
2023-06-12 14:36:29 +0300

    Java Exercise One Introduction to Java

---
+++ Exercises/co/develhope/introduction/Start.java
@@ -0,0 +1,7 @@
+package co.develhope.introduction;
+
+public class Start {
+    public static void main(String[] args){
+        System.out.println("Hello develhope java student birthday 20/01/1999");
+    }
+}
