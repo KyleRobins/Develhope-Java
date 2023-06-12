2bcfff5eefe43613eb33e537d90f57fc4357fdbc
kylerobins <darkspidersteewy@gmail.com>
2023-06-12 21:29:38 +0300

    Primitives Java

--- Lessons/Primitives.java
+++ Lessons/Primitives.java
@@ -1,7 +1,58 @@
 package Lessons;
 
-public class Variables {
+public class Primitives {
     public static void main(String[] args){
+        //Essential Primitives: int,char,boolean,double
+        //Useful Primitives: long,short,float,byte
+
+        //Essential Primitives
+
+        //Int Example;
+        int myInt = 20; //wholes numbers
+        System.out.println(myInt);
+
+        //Char Example;
+        char myChar = 'A'; // use single quotes to print character literals
+        System.out.println("char example: " + myChar);
+
+        //Boolean Example
+        boolean myBool = true; // true or false values
+        boolean myBool1 = false;
+        System.out.println(myBool);
+        System.out.println(myBool1);
+
+        //Double Example
+        double myDouble = 2.34; //8 bytes
+        System.out.println(myDouble);
+
+        //Useful Primitives
+        //Long Example
+        long myLong = 123456789L; // 8 bytes use the L to represent a long primitive
+        System.out.println(myLong);
+
+        //Short Example
+        short myShort = 2300; //2 bytes
+        System.out.println(myShort);
+
+        //Float Example
+        float myFloat = 1.2f; //Add the f to show it's a float
+        System.out.println(myFloat);
+
+        //Byte Example
+        byte myByte = 50;
+        System.out.println(myByte);
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
---
+++ Lessons/Primitives.md
@@ -0,0 +1,44 @@
+# Java Primitives
+
+## Essential Primitives
+
+### int
+- Represents whole numbers (positive and negative) without decimal points.
+- Size: 4 bytes.
+- Range: -2,147,483,648 to 2,147,483,647.
+
+### char
+- Represents a single character (letter, digit, or special character).
+- Size: 2 bytes.
+- Range: '\u0000' to '\uffff'.
+
+### boolean
+- Represents a value of either `true` or `false`.
+- Used for conditions and decision-making.
+
+### double
+- Represents decimal numbers with high precision.
+- Size: 8 bytes.
+- Range: ±4.9e-324 to ±1.8e+308.
+
+## Useful Primitives
+
+### float
+- Represents decimal numbers with lower precision than `double`.
+- Size: 4 bytes.
+- Range: ±1.4e-45 to ±3.4e+38.
+
+### short
+- Represents small whole numbers.
+- Size: 2 bytes.
+- Range: -32,768 to 32,767.
+
+### long
+- Represents larger whole numbers.
+- Size: 8 bytes.
+- Range: -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807.
+
+### byte
+- Represents small integers.
+- Size: 1 byte.
+- Range: -128 to 127.
