f98a76a1aeb7f4d4f7f33fd05d4348f17821d6b1
kylerobins <darkspidersteewy@gmail.com>
2023-06-26 10:58:29 +0300

    IF/Else Statements

---
+++ Exercises/IfElseStatements.java
@@ -0,0 +1,15 @@
+package Exercises;
+
+public class IfElseStatements {
+    public static void main(String[] args) {
+        String myName = "Kyle Robins";
+        if(myName.length() % 2 == 0){
+            System.out.println("The number of letters in my name is even");
+        } else {
+            System.out.println("The number of letters in my name is odd");
+        }
+
+    }
+
+
+}
