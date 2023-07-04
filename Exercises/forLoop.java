e4cb1951c9f31d7932c5928650f4f3a28ffd3c86
kylerobins <darkspidersteewy@gmail.com>
2023-07-04 09:48:05 +0300

    forLoop

---
+++ Exercises/forLoop.java
@@ -0,0 +1,10 @@
+package Exercises;
+//Use a for-loop to print all the number from 5 to 25 (including the upper and lower bound)
+public class forLoop {
+    public static void main(String[] arg){
+       for (int i = 5; i <= 25; i++){
+           System.out.println(i);
+       }
+
+    }
+}
