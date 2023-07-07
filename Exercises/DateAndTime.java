a627a45a0c355b75d4fd7ab85559986b947584d2
kylerobins <darkspidersteewy@gmail.com>
2023-07-07 07:17:38 +0300

    Date and Time

--- Exercises/DateAndTime.java
+++ Exercises/DateAndTime.java
@@ -1,2 +1,15 @@
-package Exercises;public class DateAndTime {
+package Exercises;
+import java.time.LocalDate;
+import java.time.Year;
+public class DateAndTime {
+    public static void main(String[] args){
+        LocalDate currentDate = LocalDate.now();
+        //calculate days left in the month
+        int daysLeftInTheMonth = currentDate.lengthOfMonth() - currentDate.getDayOfMonth();
+        System.out.println("Days left in the month:" + daysLeftInTheMonth);
+
+        //days left until the year ends
+        int daysLeftInYear = Year.of(currentDate.getYear()).length() - currentDate.getDayOfYear();
+        System.out.println("Days left till year end:" + daysLeftInYear);
+    }
 }
