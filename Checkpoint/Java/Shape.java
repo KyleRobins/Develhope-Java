aceb566657d332dc2c5bc8e86aa8c1e761ad325b
kylerobins <darkspidersteewy@gmail.com>
2023-08-23 13:21:35 +0300

    second checkpoint

--- Checkpoint/Java/Shape.java
+++ Checkpoint/Java/Shape.java
@@ -1,85 +1,63 @@
-package Java;
-interface Shape {
-    double calculateArea();
-    double calculatePerimeter();
-}
+public class Shape {
 
-class Circle implements Shape {
-    private double radius;
-
-    public Circle(double radius) {
-        this.radius = radius;
+    // Define the Drawable interface
+    interface Drawable {
+        void draw();
     }
 
-    @Override
-    public double calculateArea() {
-        return Math.PI * radius * radius;
+    // Implement Circle class
+    static class Circle implements Drawable {
+        private double radius;
+
+        public Circle(double radius) {
+            this.radius = radius;
+        }
+
+        @Override
+        public void draw() {
+            System.out.println("Drawing a circle with radius " + radius);
+        }
     }
 
-    @Override
-    public double calculatePerimeter() {
-        return 2 * Math.PI * radius;
-    }
-}
+    // Implement Square class
+    static class Square implements Drawable {
+        private double sideLength;
 
-class Rectangle implements Shape {
-    private double width;
-    private double height;
+        public Square(double sideLength) {
+            this.sideLength = sideLength;
+        }
 
-    public Rectangle(double width, double height) {
-        this.width = width;
-        this.height = height;
-    }
-
-    @Override
-    public double calculateArea() {
-        return width * height;
+        @Override
+        public void draw() {
+            System.out.println("Drawing a square with side length " + sideLength);
+        }
     }
 
-    @Override
-    public double calculatePerimeter() {
-        return 2 * (width + height);
-    }
-}
+    // Implement Triangle class
+    static class Triangle implements Drawable {
+        private double base;
+        private double height;
 
-class Triangle implements Shape {
-    private double side1;
-    private double side2;
-    private double side3;
+        public Triangle(double base, double height) {
+            this.base = base;
+            this.height = height;
+        }
 
-    public Triangle(double side1, double side2, double side3) {
-        this.side1 = side1;
-        this.side2 = side2;
-        this.side3 = side3;
-    }
-
-    @Override
-    public double calculateArea() {
-        double s = (side1 + side2 + side3) / 2;
-        return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+        @Override
+        public void draw() {
+            System.out.println("Drawing a triangle with base " + base + " and height " + height);
+        }
     }
 
-    @Override
-    public double calculatePerimeter() {
-        return side1 + side2 + side3;
-    }
-}
-
-public class ShapeDemo {
     public static void main(String[] args) {
-        Circle circle = new Circle(5);
-        Rectangle rectangle = new Rectangle(4, 6);
-        Triangle triangle = new Triangle(3, 4, 5);
+        // Create instances of the shapes
+        Drawable circle = new Circle(5);
+        Drawable square = new Square(4);
+        Drawable triangle = new Triangle(3, 6);
 
-        displayShapeInfo(circle);
-        displayShapeInfo(rectangle);
-        displayShapeInfo(triangle);
-    }
-
-    public static void displayShapeInfo(Shape shape) {
-        System.out.println("Shape: " + shape.getClass().getSimpleName());
-        System.out.println("Area: " + shape.calculateArea());
-        System.out.println("Perimeter: " + shape.calculatePerimeter());
-        System.out.println();
+        // Call the draw() method on each shape
+        circle.draw();
+        square.draw();
+        triangle.draw();
     }
 }
