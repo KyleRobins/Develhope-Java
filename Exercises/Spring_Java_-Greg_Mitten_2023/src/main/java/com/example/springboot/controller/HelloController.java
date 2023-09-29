43e0a132b61a82f4763f0dfb0107025bb7545996
kylerobins <darkspidersteewy@gmail.com>
2023-09-29 13:19:41 +0300

    Springboot Exercises

--- Exercises/Spring_Java_-Greg_Mitten_2023/src/main/java/com/example/springboot/controller/HelloController.java
+++ Exercises/Spring_Java_-Greg_Mitten_2023/src/main/java/com/example/springboot/controller/HelloController.java
@@ -1,6 +1,9 @@
 package com.example.springboot.controller;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Random;
 
 @RestController
 public class HelloController {
@@ -8,5 +11,25 @@ public class HelloController {
 	@GetMapping("/hello")
 	public String hello() {
 		return "Hello World!";
+	}
+
+	@GetMapping("/greeting")
+	public ResponseEntity<String> greeting() {
+		return ResponseEntity.ok("Good Afternoon!");
+	}
+
+	@GetMapping("/info")
+	public ResponseEntity<String> info() {
+		return ResponseEntity.ok("Information retrieved successfully!");
+	}
+
+	@GetMapping("/random")
+	public ResponseEntity<String> random() {
+		boolean result = new Random().nextBoolean();
+		if (result) {
+			return ResponseEntity.ok("200 OK");
+		} else {
+			return ResponseEntity.status(400).body("400 Bad Request");
+		}
 	}
 }
