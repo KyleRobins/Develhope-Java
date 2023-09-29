0864a4405d74555b18d497ad3fda735c9a5de8cc
kylerobins <darkspidersteewy@gmail.com>
2023-09-29 13:37:51 +0300

    Meal Controller

--- Exercises/Spring_Java_-Greg_Mitten_2023/src/main/java/com/example/springboot/controller/MealController.java
+++ Exercises/Spring_Java_-Greg_Mitten_2023/src/main/java/com/example/springboot/controller/MealController.java
@@ -1,57 +1,62 @@
 package com.example.springboot.controller;
 
-import com.example.springboot.model.Ingredient;
-import com.example.springboot.model.Meal;
-import com.example.springboot.service.MealService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
 import java.util.List;
 
+class Meal {
+    private String name;
+    private double price;
+    private String description;
+
+    public Meal(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    // getters and setters
+
+    @Override
+    public String toString() {
+        return "Meal{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+}
+
 @RestController
-@RequestMapping("/api/meal")
 public class MealController {
-    private MealService mealService;
 
-    @Autowired
-    public MealController(MealService mealService) {
-        this.mealService = mealService;
+    @GetMapping("/meals")
+    public List<Meal> getMeals() {
+        return Arrays.asList(new Meal("Breakfast", 5.99, "Morning meal"),
+                new Meal("Lunch", 10.99, "Afternoon meal"),
+                new Meal("Dinner", 15.99, "Evening meal"));
     }
 
-    @GetMapping("/list")
-    public ResponseEntity<List<Meal>> getMeals() {
-        return ResponseEntity.ok(mealService.getMeals());
+    @GetMapping("/meal/{name}")
+    public Meal getMealByName(@PathVariable String name) {
+        // Retrieve the meal by name from the database or any data source
+        return new Meal(name, 10.99, "Description for " + name);
     }
 
-    @PutMapping("/meal")
-    public ResponseEntity<Meal> addMeal(@RequestBody Meal meal) {
-        mealService.addMeal(meal);
-        return ResponseEntity.ok(meal);
+    @GetMapping("/meal/description-match/{phrase}")
+    public Meal getMealByDescription(@PathVariable String phrase) {
+        // Retrieve the meal by description from the database or any data source
+        return new Meal("Meal with description: " + phrase, 12.99, phrase);
     }
 
-    @DeleteMapping("/meal/{id}")
-    public ResponseEntity<Meal> deleteMeal(@PathVariable long id) {
-        mealService.deleteMeal(id);
-        return ResponseEntity.ok().build();
+    @GetMapping("/meal/price")
+    public List<Meal> getMealsByPriceRange(@RequestParam double min, @RequestParam double max) {
+        // Retrieve meals within the specified price range from the database or any data source
+        return Arrays.asList(new Meal("Breakfast", 5.99, "Morning meal"),
+                new Meal("Lunch", 10.99, "Afternoon meal"));
     }
-
-    @PostMapping("/meal")
-    public ResponseEntity<Meal> updateMeal(@RequestBody Meal meal) {
-        mealService.updateMeal(meal);
-        return ResponseEntity.ok(meal);
-    }
-
-    @GetMapping("/summer-meals")
-    public ResponseEntity<List<Meal>> getSummerMeals() {
-        return ResponseEntity.ok(mealService.getSummerMeals());
-    }
-
-    @GetMapping("/secret-formula")
-    public ResponseEntity<String> getSecretFormula() {
-        return ResponseEntity.ok("2 + 2 = 4");
-    }
-
 }
